32ee00052086acdf0f23a7a7615110cdc23a3b8f
AyARL <ayamrau@gmail.com>
2014-08-31 18:50:36 +0100

    Toggle Example first working version

--- DefaultEvents/Assets/ToggleExample.cs
+++ DefaultEvents/Assets/ToggleExample.cs
@@ -1,15 +1,33 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
 using System.Collections;
 
-public class ToggleExample : MonoBehaviour {
+public class ToggleExample : MonoBehaviour
+{
+    // Set in editor
+    [SerializeField]
+    private Toggle toggle = null;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    [SerializeField]
+    private Text toggleStatusText = null;
+
+    UnityAction<bool> action = null;    // The Toggle.OnValueChanged event sends one bool argument that the action needs to accept
+
+    // Use this for initialization
+    void Start()
+    {
+        toggleStatusText.text = toggle.isOn.ToString(); // grab the inital toggle value;
+
+
+        // create an action to attach to the toggle
+        action = (value) => OnToggleChange(value);  // value represents the argument that will be passed from Toggle.OnValueChanged
+
+        toggle.onValueChanged.AddListener(action);
+    }
+
+    private void OnToggleChange(bool value)
+    {
+        toggleStatusText.text = value.ToString();
+    }
 }

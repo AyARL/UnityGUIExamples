6ab8f68d60217b35664e640d7a94b50d2f2fdd73
AyARL <ayamrau@gmail.com>
2014-08-31 17:50:07 +0100

    Project Setup

---
+++ DefaultEvents/Assets/ButtonExample.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ButtonExample : MonoBehaviour
+{
+    [SerializeField]
+    private Button button = null;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
